f2096394f128c28c574ed1a8d742ddc7cee83050
Quadas <musadaq23@hotmail.com>
2023-05-14 23:18:39 -0400

    fixed a breach

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://musadaq23:Lebronmu27@socialnetworkdb.eturaox.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://127.0.0.1:27017/SocialNetworkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
